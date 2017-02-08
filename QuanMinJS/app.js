7cb0326e25cebc3442149302b505e75097d089cf
hpp <hpp@bearjoy.com>
2017-02-08 18:27:46 +0800

    session

--- QuanMinJS/app.js
+++ QuanMinJS/app.js
@@ -1,10 +1,10 @@
 //app.js
+const config = require('/config.js');
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+
   },
   getUserInfo:function(cb){
     var that = this
@@ -13,13 +13,28 @@ App({
     }else{
       //调用登录接口
       wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
+        success: function (res) {
+          if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: "https://"+config.host+"/cosphp/onlogin.php",
+            data: {
+              code: res.code
+            },success: function (res) {
+              //res:{"session_key":"WI9jwEZQvAhaTKs5YdldGQ==","expires_in":2592000,"openid":"ov5sI0Y-0t8Lxqvx2LMuFOO3VVSk"}
+               that.globalData.openid = res.data.openid;
+                wx.getUserInfo({
+                success: function (res) {
+                  that.globalData.userInfo = res.userInfo;
+                  typeof cb == "function" && cb(that.globalData.userInfo)
+                }
+              })
             }
           })
+        }
+        else{
+          console.log("获取code失败");
+        }
         }
       })
     }
@@ -28,6 +43,8 @@ App({
     userInfo:null,
     shopList:null,
     ip:"https://61652509.aimei1314.com/pp",
+    isUser:true,
+    openid:null
   },
   getUrl(route) {
       return "https://61652509.aimei1314.com/wximage/routes/album/handlers/"+route+".js";
--- QuanMinJS/pages/register/register.js
+++ QuanMinJS/pages/register/register.js
@@ -1,13 +1,16 @@
 var app = getApp();
 Page({
   data:{
-    text:"Page login"
+    userid:null
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
   onReady:function(){
     // 页面渲染完成
+    var openid = app.globalData.openid;
+    console.log(openid);
+    this.setData({userid:openid})
   },
   onShow:function(){
     // 页面显示
@@ -17,5 +20,8 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
   }
 })
